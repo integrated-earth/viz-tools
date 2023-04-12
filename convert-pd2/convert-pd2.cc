ef6ef500a475ded6ac1019015f130b026d1731c2
Pengfei Jia <pengfej@clemson.edu>
2023-04-12 10:43:01 -0400

    improve code

--- convert-pd2/convert-pd2.cc
+++ convert-pd2/convert-pd2.cc
@@ -3,17 +3,12 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-// #include <deal.II/base/exceptions.h>  // if necessary
 
 using namespace dealii;
 
 template <int dim>
 void
-converting_data_type(std::string filename);
-
-template <int dim>
-void
-converting_data_type(std::string filename)
+convert(const std::string filename)
 {
   // Reading pd2 file using read_while_parallel_file
   DataOutReader<dim, dim> data_out_reader;
@@ -21,8 +16,9 @@ converting_data_type(std::string filename)
   data_out_reader.read_whole_parallel_file(input);
 
   // Changing the extension of filename to vtu.
-  filename.replace(filename.length() - 3, 3, "vtu");
-  std::ofstream output(filename);
+  std::string pd2_filename = filename;
+  pd2_filename.replace(filename.length() - 3, 3, "vtu");
+  std::ofstream output(pd2_filename);
   data_out_reader.write_vtu(output);
 }
 
@@ -30,17 +26,36 @@ converting_data_type(std::string filename)
 int
 main(int argc, char *argv[])
 {
-  // Checking argument. If there're two arguments then argc = 3;
-  // AssertDimension(argc, 3);
+  if (argc == 3)
+    {
+      std::string pd2_filename = argv[2];
+      if (atoi(argv[1]) == 2)
+        {
+          convert<2>(pd2_filename);
+          std::cout << "Successfully converted! \n";
+        }
+      else if (atoi(argv[1]) == 3)
+        {
+          convert<3>(pd2_filename);
+          std::cout << "Successfully converted! \n";
+        }
+      else
+        {
+          std::cout << "Wrong dimension! " << std::endl;
+          std::cout << "Usage: \n \t"
+                    << "./convert_pd2 <dim> <filename>" << std::endl;
+          return 0;
+        }
+    }
+  else
+    {
+      std::cout << "Wrong number of arguments! \n ";
+      std::cout << "Usage: \n \t"
+                << "./convert_pd2 <dim> <filename>" << std::endl;
+      return 0;
+    }
 
-  std::string pd2_filename = argv[1];
-  std::cout << "filename is " << pd2_filename << std::endl;
-  // If we need more format :
-  // std::string output_type = argv[2];
-  // then adding output_type argument to convert function.
 
-  // Make sure the dimension are the same.
-  converting_data_type<3>(pd2_filename);
 
   return 0;
 }
